df88b1f3b5ac60b98fcf6d7b654e1a636a0cdfd5
DKMitt <dkmitt@sbcglobal.net>
2017-04-29 02:04:17 -0500

    friends data & survey questions

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+[{
+  "name":"Kathy",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      1,
+      3,
+      2,
+      2,
+      3,
+      4,
+      3,
+      5,
+      5,
+      3
+    ]
+},
+{
+  "name":"Bob",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      4,
+      1,
+      1,
+      2,
+      3,
+      1,
+      2,
+      3,
+      1,
+      1
+    ]
+}]
